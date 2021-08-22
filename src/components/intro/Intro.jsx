4cc8cbeb9115f9669b5e55144e8fb3b868377d95
Joshua Siuagan <joshuasiuagan0406@gmail.com>
2021-08-23 01:51:34 +0800

    Finish intro part

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import {useEffect,useRef} from 'react'
 import "./intro.scss"
+import { init  } from 'ityped';
 
 
-export default function intro() {
+export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current,{
+            showCursor:true,
+            backDelay:2500,
+            backSpeed:60,
+            strings:["Asp.Net Web API","C#","Xamarin/Xaml","Azure","React"]
+
+
+        })
+    }, []);
     return (
         <div id="intro" className="intro">
-            
+            <div className="left">
+                <div className="imageContainer">
+                    <img src="assets/Resume-photo-no-bg.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Joshua D. Siuagan</h1>
+                    <h3>Software Engineer: <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png"/>
+                </a>
+            </div>
         </div>
     )
 }
