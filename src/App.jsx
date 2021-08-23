e5f7a2fdeb22ef7eba7e2eab3be3684893b69000
Joshua Siuagan <joshuasiuagan0406@gmail.com>
2021-08-24 04:17:24 +0800

    Make the scrolling possible

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,46 @@ import { useState } from "react";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [technicalSkillActive, setTechnicalSkillActive] = useState(false);
+
+  const [menuActive, setMenuActive] = useState('intro');
+
+  const menuActiveChanger = (e) =>{
+    let clientHeight = e.target.clientHeight;
+    let scrollTop = e.target.scrollTop;
+    if(clientHeight * 0 == scrollTop)
+    {
+      console.log('intro')
+      setMenuActive('intro')
+    }
+    else if(clientHeight * 1 == scrollTop)
+    {
+      console.log('techSkills')
+      setMenuActive('techSkills')
+    }
+    else if(clientHeight * 2 == scrollTop)
+    {
+      console.log('works')
+      setMenuActive('works')
+    }  
+    else if(clientHeight * 3 == scrollTop)
+    {
+      console.log('testimonials')
+      setMenuActive('testimonials')
+    }
+    else if(clientHeight * 4 == scrollTop)
+    {
+      console.log('contact')
+      setMenuActive('contact')
+    }
+  }
+
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-      <div className="sections">
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} menuActive={menuActive}/>
+      <div onScroll={menuActiveChanger} className="sections">
         <Intro/>
-        <TechSkills technicalSkillActive={technicalSkillActive} setTechnicalSkillActive={setTechnicalSkillActive} />
+        <TechSkills/>
         <Works/>
         <Testimonials/>
         <Contact/>
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import "./menu.scss";
-export default function Menu({menuOpen,setMenuOpen}) {
+export default function Menu({menuOpen,setMenuOpen,menuActive}) {
+    console.log('Menu: '+menuActive);
     return (
         <div className={"menu "+ (menuOpen && "active")}>
             <ul>
-                <li onClick={()=>setMenuOpen(!menuOpen)}>
+                <li className={menuActive == 'intro' ? 'active': ''} onClick={()=>setMenuOpen(!menuOpen)}>
                     <a href="#intro">Home</a>
                 </li>
                 
-                <li onClick={()=>setMenuOpen(!menuOpen)}>
+                <li className={menuActive =='techSkills' ? 'active': ''} onClick={()=>setMenuOpen(!menuOpen)}>
                     <a href="#techSkills">Technical Skills</a>
                 </li>
                 
-                <li onClick={()=>setMenuOpen(!menuOpen)}>
+                <li className={menuActive =='works' ? 'active': ''} onClick={()=>setMenuOpen(!menuOpen)}>
                     <a href="#works">Works</a>
                 </li>
                 
-                <li onClick={()=>setMenuOpen(!menuOpen)}>
+                <li className={menuActive =='testimonials' ? 'active': ''}  onClick={()=>setMenuOpen(!menuOpen)}>
                     <a href="#testimonials">Testimonials</a>
                 </li>
 
-                <li onClick={()=>setMenuOpen(!menuOpen)}>
+                <li className={menuActive =='contact' ? 'active': ''} onClick={()=>setMenuOpen(!menuOpen)}>
                     <a href="#contact">Contact</a>
                 </li>
 
